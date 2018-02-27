681348edc93e800871e1749aa81f0c50ca257b21
KirylNik <ultramylo@gmail.com>
2018-02-27 23:25:53 +0300

    Everything except the degree in a few numbers

--- src/index.js
+++ src/index.js
@@ -1,27 +1,38 @@
 class SmartCalculator {
-  constructor(initialValue) {
-    // your implementation
-  }
+    constructor(value) {
+        this.command = '' + value;  // Тут будем накапливать команду.
+        this.array = []; // Массив для сохранения всех комманд.
+        this.ValueOf =  function() { return eval(this.command) };
+        this.toString =  function() { return eval(this.command) };
+    }
 
-  add(number) {
-    // your implementation
-  }
-  
-  subtract(number) {
-    // your implementation
-  }
+    add(number) {
+        this.command = this.command + ' + ' + number;
+        return this;
+    }
 
-  multiply(number) {
-    // your implementation
-  }
+    subtract(number) {
+        this.command = this.command + ' - ' + number;
+        return this;
+    }
 
-  devide(number) {
-    // your implementation
-  }
+    multiply(number) {
+        this.command = this.command + ' * ' + number;
+        return this;
+    }
 
-  pow(number) {
-    // your implementation
-  }
-}
+    devide(number) {
+        this.command = this.command + ' / ' + number;
+        return this;
+    }
 
+    pow(number) {
+        this.tempIndexOf = this.command.lastIndexOf(" ");
+        this.temp = this.command.substring((this.tempIndexOf) + 1);
+        this.temp = (+this.temp) * (+this.temp);
+        this.command = this.command.slice(0, this.tempIndexOf + 1);
+        this.command = this.command + this.temp;
+        return this;
+    }
+};
 module.exports = SmartCalculator;
