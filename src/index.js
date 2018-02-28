b7a1dce2f5300c6124b7a7c6dad3a3cccf7d0178
KirylNik <ultramylo@gmail.com>
2018-02-28 11:07:30 +0300

    done 100%

--- src/index.js
+++ src/index.js
@@ -1,38 +1,48 @@
 class SmartCalculator {
     constructor(value) {
         this.command = '' + value;  // Тут будем накапливать команду.
-        this.array = []; // Массив для сохранения всех комманд.
-        this.ValueOf =  function() { return eval(this.command) };
-        this.toString =  function() { return eval(this.command) };
+        this.pow1 = []; // Тут будем отмечать, была ли команда pow(1) до выполняемой команды
+        this.toString =  function() { return eval(this.command) }; // Выполнить команду при сравнении с чем-то.
     }
 
     add(number) {
-        this.command = this.command + ' + ' + number;
-        return this;
+        this.pow1.length = 0;  // Если была выполнена эта команда, то обнолить метку pow1
+        this.command = this.command + ' + ' + number;  // Добавить к строке с коммандой новое значение.
+        return this;  // Вернуть экземпляр себя, на случай дальнейших комманд.
     }
 
     subtract(number) {
-        this.command = this.command + ' - ' + number;
-        return this;
+        this.pow1.length = 0;  // Если была выполнена эта команда, то обнолить метку pow1
+        this.command = this.command + ' - ' + number;  // Добавить к строке с коммандой новое значение.
+        return this;  // Вернуть экземпляр себя, на случай дальнейших комманд.
     }
 
     multiply(number) {
-        this.command = this.command + ' * ' + number;
-        return this;
+        this.pow1.length = 0;  // Если была выполнена эта команда, то обнолить метку pow1
+        this.command = this.command + ' * ' + number;  // Добавить к строке с коммандой новое значение.
+        return this;  // Вернуть экземпляр себя, на случай дальнейших комманд.
     }
 
-    devide(number) {
-        this.command = this.command + ' / ' + number;
-        return this;
+    devide(number) {  // Если была выполнена эта команда, то обнолить метку pow1
+        this.pow1.length = 0;
+        this.command = this.command + ' / ' + number;  // Добавить к строке с коммандой новое значение.
+        return this;  // Вернуть экземпляр себя, на случай дальнейших комманд.
     }
 
     pow(number) {
-        this.tempIndexOf = this.command.lastIndexOf(" ");
-        this.temp = this.command.substring((this.tempIndexOf) + 1);
-        this.temp = (+this.temp) * (+this.temp);
-        this.command = this.command.slice(0, this.tempIndexOf + 1);
-        this.command = this.command + this.temp;
-        return this;
+        if (number == 1) { // Если стпень 1,то ничего не возводить в степень, но записать в буфер факт этого.
+            this.tempIndexOf = this.command.lastIndexOf(" ");
+            this.temp = this.command.substring((this.tempIndexOf) + 1);
+            this.pow1.push(this.temp);
+        } else if (this.pow1.length == 0) // Если степень не еденица.
+            for (var i = 1; i < number; i++) {  // Возводить в степень количество раз, указанное в ней.
+                this.tempIndexOf = this.command.lastIndexOf(" ");  // Получить индекс начала последнего числа в команде.
+                this.temp = this.command.substring((this.tempIndexOf) + 1); // Получить само последнее число в команде.
+                this.temp = (+this.temp) * (+this.temp); // Умножить число само на себя, возведя таким образом 1 раз в степень
+                this.command = this.command.slice(0, this.tempIndexOf + 1); // Убрать старое невозведённое последнее число.
+                this.command = this.command + this.temp;  // Добавить результат к команде
+            }
+        return this;  // Вернуть экземпляр себя, на случай дальнейших комманд.
     }
 };
 module.exports = SmartCalculator;
